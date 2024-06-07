5d734eb0a5a855dfabbef336ce629cc531d30577
Cameron Chardukian <camchardukian@gmail.com>
2024-06-07 18:22:02 -0500

    finished through 10.6

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,22 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> inputs = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (Integer.valueOf(input) < 0) {
+                break;
+            }
+            inputs.add(input);
+        }
+        inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .forEach(value -> {
+                    if (value >= 1 && value <= 5) {
+                        System.out.println(value);
+                    }
+                });
     }
 }
+// After this, print all the numbers the
+// user has given as input that are between 1 and 5.
