7ac3db9031a7830a75e3e9306342ca6ca70a3b3e
Cameron Chardukian <camchardukian@gmail.com>
2024-06-09 11:33:05 -0500

    finished through 10.14

--- part10-Part10_14.Literature/src/main/java/MainProgram.java
+++ part10-Part10_14.Literature/src/main/java/MainProgram.java
@@ -1,11 +1,27 @@
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Book> books = new ArrayList<>();
+        while (true) {
+            System.out.println("Input the name of the book, empty stops:");
+            String title = scanner.nextLine();
+            if (title.equals("")) {
+                break;
+            }
+            System.out.println("Input the age recommendation:");
+            int ageRecommendation = Integer.valueOf(scanner.nextLine());
+            books.add(new Book(title, ageRecommendation));
+        }
+        System.out.println(books.size() + " books in total.");
+        System.out.println("Books:");
+        books.sort(Comparator.comparingInt(Book::getAgeRecommendation).thenComparing(Book::getTitle));
+        books.forEach(System.out::println);
     }
 
 }
